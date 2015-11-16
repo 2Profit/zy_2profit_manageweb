f444d40f376788a51e3dd5ea2dd5b0cc853d12b4
vivian123an <543201721@qq.com>
2015-11-16 19:54:12 +0800

    数据库初始脚本mysql

--- WebContent/static/sql/base_resource_sql.sql
+++ WebContent/static/sql/base_resource_sql.sql
@@ -1,6 +1,34 @@
-INSERT INTO `org_user` VALUES ('2c9025a54d4ffb4a014d4ffe3a670001', '2015-09-06 12:56:59','1', '2015-09-06 12:56:59', null, '0', null, '2015-09-20 00:51:15', null, null, 'E10ADC3949BA59ABBE56E057F20F883E', '超级管理员', null, 'admin');
-INSERT INTO `resource` VALUES ('2c9083d14fa0fb46014fa10813be0004', null,null, '2015-09-19 23:16:58', null, '0', null, '2015-09-19 23:17:03', '系统管理', 'module', 'javascript:void(0);', null);
-INSERT INTO `resource` VALUES ('2c9083d14fa0fb46014fa10813be0005', null,null, '2015-09-19 23:18:05', null, '0', null, '2015-09-19 23:18:15', '用户管理', 'page', '/boss/user/list', '2c9083d14fa0fb46014fa10813be0004');
-INSERT INTO `resource` VALUES ('2c9083d14fa0fb46014fa10813be0006', null,null, '2015-09-19 23:20:03', null, '0', null, '2015-09-19 23:20:09', '修改密码', 'page', '/boss/user/updatePwd', '2c9083d14fa0fb46014fa10813be0004');
-INSERT INTO `resource` VALUES ('2c9083d14fa0fb46014fa10813be0007', null,null, '2015-09-19 23:18:05', null, '0', null, '2015-09-19 23:18:15', '角色管理', 'page', '/boss/role/list', '2c9083d14fa0fb46014fa10813be0004');
-INSERT INTO `resource` VALUES ('2c9083d14fa0fb46014fa10813be0008', null,null, '2015-09-19 23:18:05', null, '0', null, '2015-09-19 23:18:15', '资源管理', 'page', '/boss/resource/list', '2c9083d14fa0fb46014fa10813be0004');
+INSERT INTO 'resource' VALUES ('1', NULL, NULL, NULL, 0, NULL, '经纪商模块', 'module', NULL, NULL, 3000);
+INSERT INTO 'resource' VALUES ('6', NULL, NULL, NULL, 0, NULL, '公告模块', 'module', NULL, NULL, 5000);
+INSERT INTO 'resource' VALUES ('3', NULL, NULL, NULL, 0, NULL, '投票模块', 'module', NULL, NULL, 4000);
+INSERT INTO 'resource' VALUES ('8', NULL, NULL, NULL, 0, NULL, '账户模块', 'module', NULL, NULL, 2000);
+INSERT INTO 'resource' VALUES ('8a2a08425076316b015078b78fcb0000', '2015-10-18 10:11:47.266', NULL, NULL, 0, '2015-10-18 10:11:47.275', '支付平台', 'module', 'javascript:void(0);', NULL, NULL);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93bb5700001', NULL, NULL, NULL, 0, NULL, '系统管理', 'module', 'javascript:void(0);', NULL, 1000);
+INSERT INTO 'resource' VALUES ('402881e5503bcb6601503bcd27580000', '2015-10-6 14:18:32.134', NULL, NULL, 0, '2015-10-6 14:18:32.152', '资金管理', 'module', 'javascript:void(0);', NULL, NULL);
+
+
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93c52650002', NULL, NULL, NULL, 1, NULL, '部门管理', 'page', '/boss/department/list', '2c9081294dc93abd014dc93bb5700001', 1001);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93cb5ad0003', NULL, NULL, NULL, 0, NULL, '用户管理', 'page', '/boss/user/list', '2c9081294dc93abd014dc93bb5700001', 1002);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93d0cba0004', NULL, NULL, NULL, 0, NULL, '角色管理', 'page', '/boss/role/list', '2c9081294dc93abd014dc93bb5700001', 1003);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93d6f4c0005', NULL, NULL, NULL, 0, NULL, '资源管理', 'page', '/boss/resource/list', '2c9081294dc93abd014dc93bb5700001', 1004);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93e933f0006', NULL, NULL, NULL, 1, NULL, '登录管理', 'page', '/boss/log/list', '2c9081294dc93abd014dc93bb5700001', 1005);
+INSERT INTO 'resource' VALUES ('2c9081294dc93abd014dc93f2dca0007', NULL, NULL, NULL, 0, NULL, '修改密码', 'page', '/boss/user/updatePwd', '2c9081294dc93abd014dc93bb5700001', 1006);
+
+
+INSERT INTO 'resource' VALUES ('2', NULL, NULL, NULL, 0, NULL, '经纪商列表', 'page', '/brokerExtInfo/list', '1', 3001);
+INSERT INTO 'resource' VALUES ('4', NULL, NULL, NULL, 0, NULL, '投票主题列表', 'page', '/voteTopic/list', '3', 4001);
+INSERT INTO 'resource' VALUES ('5', NULL, NULL, NULL, 0, NULL, '投票结果列表', 'page', '/voteResult/list', '3', 4002);
+INSERT INTO 'resource' VALUES ('7', NULL, NULL, NULL, 0, NULL, '公告列表', 'page', '/notice/list', '6', 5001);
+INSERT INTO 'resource' VALUES ('9', NULL, NULL, NULL, 0, '2015-10-8 00:02:31.63', '会员管理', 'page', '/member/list', '8', 2001);
+INSERT INTO 'resource' VALUES ('402865815085d1e9015085d313360000', '2015-10-20 23:16:54.139', NULL, NULL, 0, '2015-10-20 23:32:50.212', '举报明细列表', 'page', '/postUnionReplay/report/list', '3', NULL);
+INSERT INTO 'resource' VALUES ('402881e5503bcb6601503bcdc5ae0001', '2015-10-6 14:19:12.629', NULL, NULL, 0, '2015-10-18 02:24:18.241', '虚拟币充值', 'page', '/fund/pay/list', '402881e5503bcb6601503bcd27580000', NULL);
+INSERT INTO 'resource' VALUES ('402881e550734b240150734cd4190000', '2015-10-17 08:57:06.154', NULL, NULL, 0, '2015-10-17 08:57:06.329', '会员提案', 'page', '/mem/pos/list?posStatus=0', '8', NULL);
+INSERT INTO 'resource' VALUES ('402881e5509928a10150992c5fa20000', '2015-10-24 17:27:13.365', NULL, NULL, 0, '2015-10-24 17:28:00.883', '会员证明提案', 'page', '/mem/pos/img/list?status=0', '8', NULL);
+INSERT INTO 'resource' VALUES ('8a2a08425076316b015078b7c9860001', '2015-10-18 10:12:02.05', NULL, NULL, 0, '2015-10-18 11:25:03.78', '支付平台记录', 'page', '/order/list', '8a2a08425076316b015078b78fcb0000', NULL);
+INSERT INTO 'resource' VALUES ('8a2a08425076316b015078c08c230002', '2015-10-18 10:21:36.16', NULL, NULL, 0, '2015-10-18 03:51:21.229', '虚拟币调整', 'page', '/pos/coin/list', '402881e5503bcb6601503bcd27580000', NULL);
+INSERT INTO 'resource' VALUES ('8a2a084250792ac501508414e5c40005', '2015-10-20 15:09:33.503', NULL, NULL, 0, '2015-10-20 15:09:33.508', '评论回复管理', 'page', '/postUnionReplay/list', '3', NULL);
+INSERT INTO 'resource' VALUES ('8a2a0842509a7f3d01509cbe70a80000', '2015-10-25 10:05:37.794', NULL, NULL, 0, '2015-10-25 10:05:37.832', '批量导入', 'page', '/member/import/list', '8', NULL);
+INSERT INTO 'resource' VALUES ('8a2a0842509fc9040150d326146f004c', '2015-11-4 23:38:19.627', NULL, NULL, 0, '2015-11-4 23:38:19.631', '经纪商excel导出', 'page', '/brokerExtInfo/exl/list', '1', NULL);
+
+
+
